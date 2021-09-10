6b6c2dc206f76409e3e7411a1a945c39eaa8fc39
Crowdbotics <team@crowdbotics.com>
2021-09-10 18:59:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "damp_tooth_30446Identifier",
+  url: "https://damp_tooth_30446Identifier.botics.co",
+  api: "https://damp_tooth_30446Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
